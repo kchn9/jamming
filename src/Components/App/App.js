c3adbeba41328ebe791f8209a8fbf6bc535f6c8a
kchn9 <mat.kuchnia@gmail.com>
2021-04-24 19:35:10 +0200

    Add app component

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+export class App extends React.Component {
+  render() {
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <div className="App-playlist">
+        </div>
+      </div>
+    </div>
+  }
+}
